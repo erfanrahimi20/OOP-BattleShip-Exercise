536c6c19e290552f574cf79a3b94b32c9ade8655
erfanrahimi20 <erfan.rh06@gmail.com>
2025-03-21 22:15:04 +0330

    class Player completed

---
+++ Answers/40330212032/Player.java
@@ -0,0 +1,58 @@
+
+public class Player {
+    private String name;
+    private Board board;
+    private Shiplacer[] ships;
+    private int count;
+
+    public Player(String name, Board boardSize, int maxShips) {
+        this.name = name;
+        this.board = new Board(boardSize.getSize());
+        this.ships = new Shiplacer[maxShips];
+        this.count=0;
+
+    }
+    public String getName(){
+        return name;
+    }
+    public Board getBoard(){
+        return board;
+    }
+    public void addShip(Shiplacer ship){
+        if (count < ships.length) {
+ships[count]= ship;
+count++;
+        }
+        else{
+            System.out.println("cannot add ship");
+        }
+
+    }
+
+    public boolean ifSunks(){
+        for (int i = 0; i <count ; i++) {
+            if (!ships[i].sunk()) {
+                return false;
+            }
+
+        }
+            return true;
+
+    }
+    public boolean attack(Player opponent,int row , int col){
+char[][] opponentGird=opponent.getBoard().getGrid();
+if(opponentGird[row][col]=='S'){
+    opponentGird[row][col]='X';
+    System.out.println("HIT!");
+    return true;
+}else{
+    opponentGird[row][col]='O';
+    System.out.println("MISS!");
+    return false;
+}
+
+    }
+
+
+
+}
