b5c43ccf4ec1941511b202cfa7f93f62054f4943
erfanrahimi20 <erfan.rh06@gmail.com>
2025-03-21 22:33:09 +0330

    class Player completed

--- Answers/40330212032/Shiplacer.java
+++ Answers/40330212032/Shiplacer.java
@@ -1,8 +1,10 @@
+import java.util.Random;
+
 public class Shiplacer {
     private int size;
     private int health;
-    private int startrow, startcol;
-    private int endrow, endcol;
+    private int startRow, startCol;
+    private int endRow, endCol;
 
     public Shiplacer(int size) {
 
@@ -11,13 +13,29 @@ public class Shiplacer {
 
     }
 
-    public void setLocation(int startrow, int startcol, int endrow, int endcol) {
-        this.startrow = startrow;
-        this.startcol = startcol;
-        this.endrow = endrow;
-        this.endcol = endcol;
+    public void setLocation(int startRow, int startCol, int endRow, int endCol) {
+        this.startRow = startRow;
+        this.startCol = startCol;
+        this.endRow = endRow;
+        this.endCol = endCol;
 
     }
+    public void setRandomPosition(int boardSize) {
+        Random random = new Random();
+        boolean isHorizontal = random.nextBoolean();
+        if (isHorizontal) {
+            startRow = random.nextInt(boardSize);
+            startCol = random.nextInt(boardSize - size + 1);
+            endRow = startRow;
+            endCol = startCol + size - 1;
+        }else{
+            startRow = random.nextInt(boardSize - size + 1);
+            startCol = random.nextInt(boardSize);
+            endRow = startRow + size - 1;
+            endCol = startCol;
+        }
+    }
+
 
     public void hit() {
         if (health > 0) {
