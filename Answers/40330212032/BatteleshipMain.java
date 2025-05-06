faab6fdab04ae86d4e5078c5944d1e9b514a9694
erfanrahimi20 <erfan.rh06@gmail.com>
2025-05-06 10:31:14 +0330

    completed

--- Answers/40330212032/BatteleshipMain.java
+++ Answers/40330212032/BatteleshipMain.java
@@ -16,23 +16,25 @@ public class BatteleshipMain {
 
             int choice = scanner.nextInt();
             scanner.nextLine();
-
-            if (choice == 1) {
+switch (choice){
+    case 1:
                 System.out.print("Enter your name: ");
                 String playerName = scanner.nextLine();
 
                 Game game = new Game(playerName, "AI", 10, 5, false);
                 game.start();
-            } else if (choice == 2) {
+                break;
+    case 2:
                 System.out.print("Enter Player 1 name: ");
                 String player1Name = scanner.nextLine();
 
                 System.out.print("Enter Player 2 name: ");
                 String player2Name = scanner.nextLine();
 
-                Game game = new Game(player1Name, player2Name, 10, 5, true);
-                game.start();
-            } else {
+                Game game1 = new Game(player1Name, player2Name, 10, 5, true);
+                game1.start();
+                break;
+    default:
                 System.out.println("Invalid choice. Please try again.");
             }
             }
